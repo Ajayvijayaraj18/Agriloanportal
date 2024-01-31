3890d85846b9d79d3b75a1ea7338cb36beaa52a7
Ajayvijayaraj18 <115450211+Ajayvijayaraj18@users.noreply.github.com>
2024-01-31 12:18:02 +0530

    Add files via upload

---
+++ FrontEnd/Day4/src/AdminNavbar.jsx
@@ -0,0 +1,24 @@
+
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+const AdminNavbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/adminhome">Home</Link>
+        </li>
+        <li>
+          <Link to="/alist">Loan</Link>
+        </li>
+        <li>
+          <Link to="/others">others</Link>
+        </li>
+        
+      </ul>
+    </nav>
+  );
+};
+
+export default AdminNavbar;
